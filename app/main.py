318e66a698068863f147aba670e43980ccb4804e
Abror <izzatullaev2001abror@gmail.com>
2024-11-25 11:36:25 +0500

    initial commit

--- app/main.py
+++ app/main.py
@@ -10,11 +10,6 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-
-from .admin_panel import admin_panel_apply, authentication_backend
-from .config import settings
-from .database import engine
-from .services.redis_service import get_redis, get_cache
 from .utils.middlewares import handle_integrity_errors
 from .user import routes as user_route
 from .chat import routes as chat_route
@@ -23,33 +18,33 @@ from starlette.middleware.cors import CORSMiddleware
 # from debug_toolbar.middleware import DebugToolbarMiddleware
 from sqladmin import Admin
 
-@asynccontextmanager
-async def lifespan(_app: FastAPI):
-    _app.redis = await get_redis()
+# @asynccontextmanager
+# async def lifespan(_app: FastAPI):
+#     _app.redis = await get_redis()
+    #
+    # _postgres_dsn = settings.postgres_url.unicode_string()
 
-    _postgres_dsn = settings.postgres_url.unicode_string()
+    # try:
 
-    try:
+        # redis_cache = await get_cache()
+        # FastAPICache.init(RedisBackend(redis_cache), prefix="fastapi-cache")
+        #
+        # _app.postgres_pool = await asyncpg.create_pool(
+        #     dsn = _postgres_dsn,
+        #     # min_size=20,  # Higher minimum size to quickly handle many concurrent connections
+        #     # max_size=100,  # Larger maximum size to support peak loads
+        #     # max_inactive_connection_lifetime=300,  # Recycle idle connections after 5 minutes
+        #     # timeout=60,
+        # )
+        # # await init_models()
 
-        redis_cache = await get_cache()
-        FastAPICache.init(RedisBackend(redis_cache), prefix="fastapi-cache")
+    #     yield
+    #
+    # finally:
+    #     await _app.redis.close()
+    #     await _app.postgres_pool.close()
 
-        _app.postgres_pool = await asyncpg.create_pool(
-            dsn = _postgres_dsn,
-            # min_size=20,  # Higher minimum size to quickly handle many concurrent connections
-            # max_size=100,  # Larger maximum size to support peak loads
-            # max_inactive_connection_lifetime=300,  # Recycle idle connections after 5 minutes
-            # timeout=60,
-        )
-        # await init_models()
-
-        yield
-
-    finally:
-        await _app.redis.close()
-        await _app.postgres_pool.close()
-
-app = FastAPI(lifespan=lifespan, debug=True)
+app = FastAPI(debug=True)
 # app.add_middleware(
 #     DebugToolbarMiddleware,
 #     panels=["debug_toolbar.panels.sqlalchemy.SQLAlchemyPanel"],
@@ -79,6 +74,6 @@ async def chat(request: Request):
 async def chat_page(request: Request):
     return templates.TemplateResponse("chat.html", {"request": request})
 
-admin = Admin(app=app, engine=engine, authentication_backend=authentication_backend)
+# admin = Admin(app=app, engine=engine, authentication_backend=authentication_backend)
 
-admin_panel_apply(admin)
+# admin_panel_apply(admin)
